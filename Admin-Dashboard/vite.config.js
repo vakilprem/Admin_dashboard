03edae7c850b28cce4f6ad5b989428bb109564da
vakilprem <vakilprem@gmail.com>
2025-04-09 21:38:28 +0530

    Vercel

--- Admin-Dashboard/vite.config.js
+++ Admin-Dashboard/vite.config.js
@@ -1,7 +1,10 @@
+// vite.config.js
 export default {
+  root: './src',  // Point Vite to your source directory if it's not the root
   build: {
+    outDir: '../dist', // Specify the build output directory
     rollupOptions: {
-      external: ['react-countup']
+      input: './src/index.html' // Ensure Vite knows where to look for the entry HTML file
     }
   }
 }

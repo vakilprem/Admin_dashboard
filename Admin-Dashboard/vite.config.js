256e3652c7ce6e2ceb67d9103272ccb9ac26ff6e
Premal vakil <109590610+vakilprem@users.noreply.github.com>
2025-04-09 22:07:34 +0530

    Update vite.config.js

--- Admin-Dashboard/vite.config.js
+++ Admin-Dashboard/vite.config.js
@@ -1,10 +1,6 @@
-// vite.config.js
-export default {
-  root: './src',  // Point Vite to your source directory if it's not the root
-  build: {
-    outDir: '../dist', // Specify the build output directory
-    rollupOptions: {
-      input: './src/index.html' // Ensure Vite knows where to look for the entry HTML file
-    }
-  }
-}
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+});
